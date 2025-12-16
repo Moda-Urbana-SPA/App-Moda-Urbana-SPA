56b4eb741ca63ade861120cd0cc065ceb533728a
Fabian-Farias <fab.farias@duocuc.cl>
2025-12-16 08:45:57 -0300

    render update

--- app/src/main/java/com/example/modaurbanaprototipoapp/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/modaurbanaprototipoapp/data/remote/RetrofitClient.kt
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit
 object RetrofitClient {
 
     // API de Xano
-    private const val BASE_URL = "https://modaurbana-api-hoo9.onrender.com/api/"
+    private const val BASE_URL = "https://modaurbana-api-x1ff.onrender.com/api/"
 
     fun create(context: Context): Retrofit {
 
